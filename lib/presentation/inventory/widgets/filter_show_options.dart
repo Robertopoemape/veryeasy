503fa8b5b38adea2c27173816c41e46262ef233e
roberto <poemap1@gmail.com>
2025-03-15 12:08:38 -0500

    refactor: added size

--- lib/presentation/inventory/widgets/filter_show_options.dart
+++ lib/presentation/inventory/widgets/filter_show_options.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:veryeasy/presentation/inventory/widgets/widgets.dart';
 
+import '../../../core/core.dart';
+
 void filterShowOptions(BuildContext context) {
   showModalBottomSheet(
     context: context,
     isScrollControlled: true,
     shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      borderRadius: BorderRadius.vertical(top: Radius.circular(ds20)),
     ),
     builder: (context) {
       return const InventoryFilterOptions();
